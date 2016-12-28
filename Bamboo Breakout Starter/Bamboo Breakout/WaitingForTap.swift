8505701f9f9b248fa7e90fca28a88366c7887402
Benito Sanchez <sanchezbenitog@gmail.com>
2016-12-27 20:56:19 -0800

    Restarted with starterpack. Added a bouncing ball.

---
+++ Bamboo Breakout Starter/Bamboo Breakout/WaitingForTap.swift
@@ -0,0 +1,25 @@
+//
+//  WaitingForTap.swift
+//  BreakoutSpriteKitTutorial
+//
+//  Created by Michael Briscoe on 1/16/16.
+//  Copyright © 2016 Razeware LLC. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class WaitingForTap: GKState {
+  unowned let scene: GameScene
+  
+  init(scene: SKScene) {
+    self.scene = scene as! GameScene
+    super.init()
+  }
+  
+  
+  override func isValidNextState(_ stateClass: AnyClass) -> Bool {
+    return stateClass is Playing.Type
+  }
+
+}
